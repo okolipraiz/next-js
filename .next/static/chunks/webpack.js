72d5eb9fd5f56b0024f1d9772fa97a4d412a609b
okoli praiz <okolipraiz.com>
2022-02-22 13:15:58 +0100

    updated netlify dep

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91b9e7f662103cfd"; }
+/******/ 		__webpack_require__.h = function() { return "5b26aa2d7c29b5c4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
